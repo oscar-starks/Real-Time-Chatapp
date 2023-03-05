bda4f138bc45f77e6454a0be7f611ded90bbce0b
Blessed <oscarblessed04@gmail.com>
2023-03-05 15:21:06 +0100

    commented the previous code and rewrote a functional logic

--- Real_time_chatapp/routing.py
+++ Real_time_chatapp/routing.py
@@ -7,9 +7,9 @@ django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
-    'websocket':AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
-    ),
+    # 'websocket':AuthMiddlewareStack(
+    #     URLRouter(
+    #         chat.routing.websocket_urlpatterns
+    #     )
+    # ),
 })  
--- Real_time_chatapp/settings.py
+++ Real_time_chatapp/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-ASGI_APPLICATION = "Real_time_chatapp.routing.application"   
+ASGI_APPLICATION = "Real_time_chatapp.asgi.application"   
 
 CHANNEL_LAYERS = {
     'default':  {
