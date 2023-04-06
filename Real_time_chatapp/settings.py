51deb7443367b4bd9d103c7a795f685af844c42c
oscar-starks <oscarblessed04@gmail.com>
2023-04-06 16:44:03 +0100

    switched to redis

--- Real_time_chatapp/settings.py
+++ Real_time_chatapp/settings.py
@@ -127,8 +127,17 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 ASGI_APPLICATION = "Real_time_chatapp.asgi.application"   
 
+# CHANNEL_LAYERS = {
+#     'default':  {
+#         "BACKEND":"channels.layers.InMemoryChannelLayer",
+#     }
+# }
+
 CHANNEL_LAYERS = {
-    'default':  {
-        "BACKEND":"channels.layers.InMemoryChannelLayer",
-    }
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
 }
